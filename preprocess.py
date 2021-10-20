c9f4a1be943e78507711ab981a54059341d3b50d
Alexandre Bremard <bremard.alexandre@gmail.com>
2021-10-20 09:25:39 +0200

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -4,6 +4,8 @@ import sys
 import math
 
 from datetime import datetime
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 if __name__ == "__main__":
 
@@ -64,8 +66,6 @@ if __name__ == "__main__":
             })
 
     dataset = pd.DataFrame(dataset_rows).sort_values(['pressed'])
-    print(dataset)
-    # dataset.to_csv("dataset.csv", index=False, date_format='%Y-%m-%d %H:%M:%S %f')
 
     dataset["hold_time"] = (dataset["released"] - dataset["pressed"]) / pd.Timedelta(microseconds=1)
     press_press_time = []
@@ -92,7 +92,7 @@ if __name__ == "__main__":
 
     print(dataset)
 
-    # output statistics
+    # Analyse statistique
     average_hold_time = dataset["hold_time"].mean()
     average_press_press_time = dataset.iloc[:len(dataset)-1]["press_press_time"].mean()
     average_press_release_time = dataset.iloc[:len(dataset)-1]["press_release_time"].mean()
@@ -104,3 +104,16 @@ if __name__ == "__main__":
     print("average_press_release_time", average_press_release_time)
     print("average_release_release_time", average_release_release_time)
     print("apm", apm)
+
+    dataset.to_csv("dataset.csv", index=False)
+
+    # # KNN
+    # X = dataset[dataset.columns.difference(['user', 'pressed', 'released'])]
+    # y = dataset["user"]
+    # X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
+    # ## Instantiate the model with 5 neighbors. 
+    # knn = KNeighborsClassifier(n_neighbors=5)
+    # ## Fit the model on the training data.
+    # knn.fit(X_train, y_train)
+    # ## See how the model performs on the test data.
+    # knn.score(X_test, y_test)
