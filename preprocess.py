8348b3ec145e5a2c28a63e96519eccbc50607429
Alexandre Bremard <bremard.alexandre@gmail.com>
2021-10-20 10:28:58 +0200

    bug fix

--- preprocess.py
+++ preprocess.py
@@ -19,8 +19,12 @@ if __name__ == "__main__":
         with open(press_file) as fp:
             line = fp.readline()
             while line:
+                if line == "\n":
+                    print("empty line!")
+                    line = fp.readline()
+                    continue
                 timestamp = datetime.strptime(line.strip().split(" - ")[0], '%Y-%m-%d %H:%M:%S %f')
-                key = line.strip().split(" - ")[1]
+                key = line.strip().split(" - ")[1].lower()
                 press_rows.append({
                     "timestamp": timestamp,
                     "key": key
@@ -31,8 +35,12 @@ if __name__ == "__main__":
         with open(release_file) as fp:
             line = fp.readline()
             while line:
+                if line == "\n":
+                    print("empty line release!")
+                    line = fp.readline()
+                    continue                
                 timestamp = datetime.strptime(line.strip().split(" - ")[0], '%Y-%m-%d %H:%M:%S %f')
-                key = line.strip().split(" - ")[1]
+                key = line.strip().split(" - ")[1].lower()
                 release_rows.append({
                     "timestamp": timestamp,
                     "key": key
@@ -42,6 +50,9 @@ if __name__ == "__main__":
     press_df = pd.DataFrame(press_rows)
     release_df = pd.DataFrame(release_rows)
 
+    press_df.to_csv("press_df.csv", date_format="%Y-%m-%d %H:%M:%S %f")
+    release_df.to_csv("release_df.csv", date_format="%Y-%m-%d %H:%M:%S %f")
+
     print(press_df)
     print(release_df)
 
@@ -49,13 +60,12 @@ if __name__ == "__main__":
     dataset_rows = []
     for id, row in release_df.drop_duplicates(subset=['key']).iterrows():
         sub_df = release_df[release_df["key"] == row['key']]
-        print(sub_df)
         lower_bound = None       
         for sub_id, sub_row in sub_df.reset_index().iterrows():
             higher_bound = sub_row["timestamp"]
             if sub_id == 0:
                 press_time = press_df[(press_df["key"] == sub_row["key"]) & (press_df["timestamp"] < higher_bound)].iloc[0]["timestamp"]
-            else:
+            else:             
                 press_time = press_df[(press_df["key"] == sub_row["key"]) & (press_df["timestamp"] < higher_bound) & (press_df["timestamp"] > lower_bound)].iloc[0]["timestamp"]
             lower_bound = higher_bound
             dataset_rows.append({
@@ -66,13 +76,14 @@ if __name__ == "__main__":
             })
 
     dataset = pd.DataFrame(dataset_rows).sort_values(['pressed'])
+    dataset.to_csv("tmp_dataset.csv", date_format="%Y-%m-%d %H:%M:%S %f")
 
     dataset["hold_time"] = (dataset["released"] - dataset["pressed"]) / pd.Timedelta(microseconds=1)
     press_press_time = []
     press_release_time = []
     release_release_time = []
 
-    for index, row in dataset.iterrows():
+    for index, row in dataset.reset_index().iterrows():
         press_press_delta = (dataset.iloc[index+1]["pressed"] - dataset.iloc[index]["pressed"]) / pd.Timedelta(microseconds=1)
         press_press_time.append(press_press_delta)
         release_release_delta = (dataset.iloc[index+1]["released"] - dataset.iloc[index]["released"]) / pd.Timedelta(microseconds=1)
